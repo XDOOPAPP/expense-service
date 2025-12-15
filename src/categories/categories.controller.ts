f0239e353e980d3610020ece354a6f39c3b6ef56
phamson1928 <pson4282@gmail.com>
2025-12-16 00:48:55 +0700

    feat: Add categories controller with a `findAll` method for retrieving categories.

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,15 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { CategoriesService } from './categories.service';
-
+import { Auth } from '../common/decorators/auth.decorator';
+import { MessagePattern } from '@nestjs/microservices';
 @ApiTags('Categories')
 @Controller('expenses/categories')
 export class CategoriesController {
-  constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(private readonly categoriesService: CategoriesService) { }
 
-  @Get()
-  @ApiOperation({ summary: 'Get all expense categories' })
-  @ApiResponse({ status: 200, description: 'Returns all categories' })
+  @MessagePattern('category.findAll')
+  @Auth()
   async findAll() {
     return this.categoriesService.findAll();
   }
